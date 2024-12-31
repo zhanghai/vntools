1a9ebadb970abb5be43046ce7cf2db1b60787b89
Hai Zhang <dreaming.in.code.zh@gmail.com>
2024-12-31 13:07:15 -0800

    Feat: Add iga2vnmzip

---
+++ iga2vnmzip/iga2vnmzip.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+help() {
+    echo 'Usage: iga2vnmzip FLOWERS_DIRECTORY OUTPUT.vnm.zip' >&2
+}
+
+data_to_name() {
+    case "$1" in
+        data00)
+            echo script
+            ;;
+        data01)
+            echo fgimage
+            ;;
+        data02)
+            echo bgimage
+            ;;
+        data03)
+            echo system
+            ;;
+        data04)
+            echo bgm
+            ;;
+        *)
+            echo "Unknown data $1" >&2
+            exit 1
+            ;;
+    esac
+}
+
+main() {
+    if [[ $# -ne 2 || ! -d "$1" || "$2" != *.vnm.zip ]]; then
+        help
+        exit 1
+    fi
+
+    if [[ ! (-d "$1/%DEFAULT FOLDER%" || -f "$1/data00.iga") ]]; then
+        echo 'Missing unpacked directory or file' >&2
+        exit 1
+    fi
+
+    temp_dir="$(mktemp -d)"
+    trap 'rm -rf -- "$temp_dir"' EXIT
+
+    for f in "$1/"*.iga; do
+        if [[ "$f" == *data*.iga ]]; then
+            continue
+        fi
+        echo "Extracting $f..."
+        d="$temp_dir/$(basename "$f" .iga)"
+        mkdir "$d"
+        ../igatool/igatool -x "$f" "$d"
+    done
+    if [[ -d "$1/%DEFAULT FOLDER%" ]]; then
+        for f in "$1/%DEFAULT FOLDER%/"*.iga; do
+            echo "Extracting $f..."
+            d="$temp_dir/$(data_to_name "$(basename "$f" .iga)")"
+            ../igatool/igatool -x "$f" "$d"
+        done
+    else
+        for f in "$1/"data*.iga; do
+            echo "Extracting $f..."
+            d="$temp_dir/$(data_to_name "$(basename "$f" .iga)")"
+            ../igatool/igatool -x "$f" "$d"
+        done
+    fi
+
+    echo "Converting script to VNMark..."
+    kotlin ../igs2vnm/igs2vnm.main.kts "$temp_dir/script" "$temp_dir/vnmark"
+    # Original script kept for easier debugging.
+    #rm -r "$temp_dir/script"
+
+    for d in "$temp_dir/"*/; do
+        for f in "$d"*.bmp; do
+            if [[ "$f" == *'*.bmp' ]]; then
+                break
+            fi
+            echo "Converting $f to JPEG..."
+            convert -quality 95 "$f" "${f%.bmp}.jpg"
+            rm "$f"
+        done
+    done
+    for d in "$temp_dir/"*/; do
+        for f in "$d"*.png; do
+            if [[ "$f" == *'*.png' ]]; then
+                break
+            fi
+            echo "Converting $f to WEBP..."
+            convert -quality 95 "$f" "${f%.png}.webp"
+            rm "$f"
+        done
+    done
+
+    output="$(realpath "$2")"
+    echo "Creating $output..."
+    rm -f "$output"
+    (cd "$temp_dir"; zip -0DrX "$output" .)
+}
+
+main "$@"
